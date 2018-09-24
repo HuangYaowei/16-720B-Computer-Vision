90638d4026acbe98872be41314ff0a1841086860
Heethesh Vhavle <heethesh@gmail.com>
2018-09-24 15:32:32 -0400

    Completed VGG layers and feature extraction till fc7

--- Assignments/HW1/code/deep_recog.py
+++ Assignments/HW1/code/deep_recog.py
@@ -1,100 +1,131 @@
-import numpy as np
+import os
 import multiprocessing
-import threading
-import queue
-import imageio
-import os,time
+
 import torch
+import imageio
+import numpy as np
 import skimage.transform
 import torchvision.transforms
+
 import util
 import network_layers
 
-def build_recognition_system(vgg16,num_workers=2):
-	'''
-	Creates a trained recognition system by generating training features from all training images.
+# Standardization
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
 
-	[input]
-	* vgg16: prebuilt VGG-16 network.
-	* num_workers: number of workers to process in parallel
+# PyTorch transform objects setup
+resize = torchvision.transforms.Resize((224, 224))
+normalize = torchvision.transforms.Normalize(mean, std)
+image2tensor = torchvision.transforms.ToTensor()
+tensor2image = torchvision.transforms.ToPILImage()
 
-	[saved]
-	* features: numpy.ndarray of shape (N,K)
-	* labels: numpy.ndarray of shape (N)
-	'''
+def build_recognition_system(vgg16, num_workers=2):
+    '''
+    Creates a trained recognition system by generating training features from all training images.
+
+    [input]
+    * vgg16: prebuilt VGG-16 network.
+    * num_workers: number of workers to process in parallel
+
+    [saved]
+    * features: numpy.ndarray of shape (N, K)
+    * labels: numpy.ndarray of shape (N)
+    '''
+
+    train_data = np.load("../data/train_data.npz")
+    n_train = train_data['image_names'].shape[0]
+    args = [ (i, train_data['image_names'][i][0], vgg16) for i in range(n_train) ]
+
+    get_image_feature(args[0])
+    # get_image_feature((0, ''))
+
+def evaluate_recognition_system(vgg16, num_workers=2):
+    '''
+    Evaluates the recognition system for all test images and returns the confusion matrix.
+
+    [input]
+    * vgg16: prebuilt VGG-16 network.
+    * num_workers: number of workers to process in parallel
+
+    [output]
+    * conf: numpy.ndarray of shape (8, 8)
+    * accuracy: accuracy of the evaluated system
+    '''
+
+    
+    test_data = np.load("../data/test_data.npz")
+    # ----- TODO -----
+    pass
 
 
-	train_data = np.load("../data/train_data.npz")
-	# ----- TODO -----
-	pass
+def preprocess_image(image, toTensor=True):
+    '''
+    Preprocesses the image to load into the prebuilt network.
 
+    [input]
+    * image: numpy.ndarray of shape (H, W, 3)
 
-def evaluate_recognition_system(vgg16,num_workers=2):
-	'''
-	Evaluates the recognition system for all test images and returns the confusion matrix.
+    [output]
+    * image_processed: torch.Tensor of shape (3, H, W)
+    '''
 
-	[input]
-	* vgg16: prebuilt VGG-16 network.
-	* num_workers: number of workers to process in parallel
+    # Convert grayscale images to 3 channels
+    if len(image.shape) < 3:
+        image = np.dstack([image] * 3)
 
-	[output]
-	* conf: numpy.ndarray of shape (8,8)
-	* accuracy: accuracy of the evaluated system
-	'''
+    # Convert higher channel images to 3 channels
+    if image.shape[2] > 3:
+        image = image[:, :, :3]
 
-	
-	test_data = np.load("../data/test_data.npz")
-	# ----- TODO -----
-	pass
-
-
-def preprocess_image(image):
-	'''
-	Preprocesses the image to load into the prebuilt network.
-
-	[input]
-	* image: numpy.ndarray of shape (H,W,3)
-
-	[output]
-	* image_processed: torch.Tensor of shape (3,H,W)
-	'''
-	# ----- TODO -----
-	pass
-
+    # Convert to float and normalize
+    if toTensor:
+        image_processed = normalize(scale(image2tensor(image))).unsqueeze(0)
+    else:
+        image = image.astype('float')/255
+        image = skimage.transform.resize(image, (224, 224, 3))
+        image_processed = (image - mean)/std
+        
+    return image_processed
 
 def get_image_feature(args):
-	'''
-	Extracts deep features from the prebuilt VGG-16 network.
-	This is a function run by a subprocess.
- 	[input]
-	* i: index of training image
-	* image_path: path of image file
-	* vgg16: prebuilt VGG-16 network.
-	* time_start: time stamp of start time
- 	[saved]
-	* feat: evaluated deep feature
-	'''
+    '''
+    Extracts deep features from the prebuilt VGG-16 network.
+    This is a function run by a subprocess.
 
-	i,image_path,vgg16 = args
+    [input]
+    * i: index of training image
+    * image_path: path of image file
+    * vgg16: prebuilt VGG-16 network.
+    
+    [saved]
+    * feat: evaluated deep feature
+    '''
 
-	# ----- TODO -----
-	pass
+    i, image_path, vgg16 = args
 
+    # Read image
+    image = imageio.imread('../data/' + image_path)
+    
+    # Preprocess image
+    image = preprocess_image(image, toTensor=False)
 
+    # Extract deep features
+    feat = network_layers.extract_deep_feature(image, util.get_VGG16_weights())
+    # print(feat.shape)
 
+def distance_to_set(feature, train_features):
+    '''
+    Compute distance between a deep feature with all training image deep features.
 
-def distance_to_set(feature,train_features):
-	'''
-	Compute distance between a deep feature with all training image deep features.
+    [input]
+    * feature: numpy.ndarray of shape (K)
+    * train_features: numpy.ndarray of shape (N, K)
 
-	[input]
-	* feature: numpy.ndarray of shape (K)
-	* train_features: numpy.ndarray of shape (N,K)
+    [output]
+    * dist: numpy.ndarray of shape (N)
+    '''
 
-	[output]
-	* dist: numpy.ndarray of shape (N)
-	'''
-
-	# ----- TODO -----
-	
-	pass
+    # ----- TODO -----
+    
+    pass
--- Assignments/HW1/code/network_layers.py
+++ Assignments/HW1/code/network_layers.py
@@ -1,73 +1,122 @@
+import os
+import time
+
 import numpy as np
 import scipy.ndimage
-import os,time
 
-def extract_deep_feature(x,vgg16_weights):
-	'''
-	Extracts deep features from the given VGG-16 weights.
+import util
 
-	[input]
-	* x: numpy.ndarray of shape (H,W,3)
-	* vgg16_weights: numpy.ndarray of shape (L,3)
+def extract_deep_feature(x, vgg16_weights):
+    '''
+    Extracts deep features from the given VGG-16 weights.
 
-	[output]
-	* feat: numpy.ndarray of shape (K)
-	'''
-	pass
+    [input]
+    * x: numpy.ndarray of shape (H, W, 3)
+    * vgg16_weights: numpy.ndarray of shape (L, 3)
 
+    [output]
+    * feat: numpy.ndarray of shape (K)
+    '''
 
-def multichannel_conv2d(x,weight,bias):
-	'''
-	Performs multi-channel 2D convolution.
+    ops = {'conv2d': multichannel_conv2d, 'relu': relu,
+           'maxpool2d': max_pool2d, 'linear': linear}
 
-	[input]
-	* x: numpy.ndarray of shape (H,W,input_dim)
-	* weight: numpy.ndarray of shape (output_dim,input_dim,kernel_size,kernel_size)
-	* bias: numpy.ndarray of shape (output_dim)
+    # Pass image through the different layers of VGG-16
+    for layer in vgg16_weights[:35]:
+        print('\n', layer[0], '-'*30)
+        print('Input:', x.shape)
 
-	[output]
-	* feat: numpy.ndarray of shape (H,W,output_dim)
-	'''
-	
-	pass
+        args = [x] + layer[1:] if (len(layer) > 1) else [x]
+        x = ops[layer[0]](*args)
+
+        print('Output:', x.shape)
+
+    np.save('deep_features', x)
+    print(x.shape)
+    return x
+
+def multichannel_conv2d(x, weight, bias):
+    '''
+    Performs multi-channel 2D convolution.
+
+    [input]
+    * x: numpy.ndarray of shape (H, W, input_dim)
+    * weight: numpy.ndarray of shape (output_dim, input_dim, kernel_size, kernel_size)
+    * bias: numpy.ndarray of shape (output_dim)
+
+    [output]
+    * feat: numpy.ndarray of shape (H, W, output_dim)
+    '''
+
+    y = []
+    for i in range(weight.shape[0]):
+        H = []
+        for j in range(weight.shape[1]):
+            # Flip LR the weight matrix
+            W = np.fliplr(weight[i, j, :, :])
+            # 2D convolve on each input channel
+            H.append(scipy.ndimage.convolve(x[:, :, j], W))
+        # Sum up all the convulution responses and the bias term
+        y.append(sum(H) + bias[i])
+
+    # Stack all filter responses back
+    y = np.dstack(y)
+    return y
 
 def relu(x):
-	'''
-	Rectified linear unit.
+    '''
+    Rectified linear unit.
 
-	[input]
-	* x: numpy.ndarray
+    [input]
+    * x: numpy.ndarray
 
-	[output]
-	* y: numpy.ndarray
-	'''
-	pass
+    [output]
+    * y: numpy.ndarray
+    '''
+    
+    y = np.maximum(x, 0)
+    return y
 
-def max_pool2d(x,size):
-	'''
-	2D max pooling operation.
+def max_pool2d(x, size):
+    '''
+    2D max pooling operation.
 
-	[input]
-	* x: numpy.ndarray of shape (H,W,input_dim)
-	* size: pooling receptive field
+    [input]
+    * x: numpy.ndarray of shape (H, W, input_dim)
+    * size: pooling receptive field
 
-	[output]
-	* y: numpy.ndarray of shape (H/size,W/size,input_dim)
-	'''
-	
-	pass
+    [output]
+    * y: numpy.ndarray of shape (H/size, W/size, input_dim)
+    '''
+    
+    # Make the input divisible by the pooling size
+    FH = (x.shape[0]//size) * size
+    FW = (x.shape[1]//size) * size
 
-def linear(x,W,b):
-	'''
-	Fully-connected layer.
+    # Max pool on each channel
+    y = []
+    for i in range(x.shape[2]):
+        # Reshape the array into groups of pooling size
+        temp = x[:FH, :FW, i].reshape(FH//size, size, FW//size, size)
+        # Pick the maximum value from each (size x size) block
+        y.append(temp.max(axis=(1, 3)))
 
-	[input]
-	* x: numpy.ndarray of shape (input_dim)
-	* weight: numpy.ndarray of shape (output_dim,input_dim)
-	* bias: numpy.ndarray of shape (output_dim)
+    # Stack all responses back
+    y = np.dstack(y)
+    return y
 
-	[output]
-	* y: numpy.ndarray of shape (output_dim)
-	'''
-	pass
+def linear(x, W, b):
+    '''
+    Fully-connected layer.
 
+    [input]
+    * x: numpy.ndarray of shape (input_dim)
+    * W: numpy.ndarray of shape (output_dim, input_dim)
+    * b: numpy.ndarray of shape (output_dim)
+
+    [output]
+    * y: numpy.ndarray of shape (output_dim)
+    '''
+    
+    y = np.dot(W, x.flatten()) + b
+    return y

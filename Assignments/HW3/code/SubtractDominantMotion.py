c3bd20426bff3d85dd6fbc57b8cedf377f042a8e
heethesh <heethesh@gmail.com>
2018-10-21 09:35:10 -0400

    Completed motion subtraction

---
+++ Assignments/HW3/code/SubtractDominantMotion.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+'''
+16-720B Computer Vision (Fall 2018)
+Homework 3 - Lucas-Kanade Tracking and Correlation Filters
+'''
+
+__author__ = "Heethesh Vhavle"
+__credits__ = ["Simon Lucey", "16-720B TAs"]
+__version__ = "1.0.1"
+__email__ = "heethesh@cmu.edu"
+
+import cv2
+import numpy as np
+from scipy.ndimage import affine_transform
+from scipy.ndimage.morphology import binary_opening, binary_closing
+
+from LucasKanade import disp
+from LucasKanadeAffine import LucasKanadeAffine
+
+def SubtractDominantMotion(image1, image2, threshold=0.3, iters=7):
+    '''
+    [input]
+    * image1 - Image at time t
+    * image2 - Image at time t+1
+    * threshold - Threshold for creating binary mask
+    * iters - Iterations for morphological closing
+
+    [output]
+    * mask: Binary image of input size corresponding to moving objects
+    '''
+
+    # Compute affine transform
+    M1to2 = np.flip(LucasKanadeAffine(image1, image2))[..., [1, 2, 0]]
+    M2to1 = np.linalg.inv(np.vstack([M1to2, [0, 0, 1]]))
+    M2to1 = M2to1/M2to1[-1, -1]
+
+    # Warp image
+    warp_img = affine_transform(image1, M2to1)
+    aff_mask = affine_transform(np.ones(image1.shape), M2to1)
+    
+    # Get difference image and normalize it
+    diff = np.abs((aff_mask * image2) - (aff_mask * warp_img))
+    diff = (diff - diff.min()) * (1/(diff.max() - diff.min()))
+    
+    # Create a binary mask using the theshold
+    mask = np.where(diff > threshold, 1, 0).astype('bool')
+    mask = binary_opening(binary_closing(mask, iterations=iters))
+
+    return mask
+
+if __name__ == '__main__':
+    aerialseq = np.load('../data/aerialseq.npy')
+    SubtractDominantMotion(aerialseq[:, :, 0], aerialseq[:, :, 1])
---
+++ Assignments/HW3/code/testAerialSequence.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+'''
+16-720B Computer Vision (Fall 2018)
+Homework 3 - Lucas-Kanade Tracking and Correlation Filters
+'''
+
+__author__ = "Heethesh Vhavle"
+__credits__ = ["Simon Lucey", "16-720B TAs"]
+__version__ = "1.0.1"
+__email__ = "heethesh@cmu.edu"
+
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import matplotlib.patches as patches
+
+from SubtractDominantMotion import SubtractDominantMotion
+
+def play(filename):
+    frames = np.load(filename)
+    total_frames = frames.shape[2]
+    fps = 24
+
+    # Init figure
+    fig, ax = plt.subplots(1)
+    im = ax.imshow(frames[:, :, 0], animated=True, cmap='gray')
+
+    def updatefig(j):
+        sys.stdout.write('\rFrame: %04d/%04d | FPS: %d | Sequence: [%s]%s' % (j, total_frames, fps, filename, ' '*10))
+
+        # Update image for display
+        ax.clear()
+        im = ax.imshow(frames[:, :, j], cmap='gray')
+
+        # Get motion detection mask and display
+        mask = SubtractDominantMotion(frames[:, :, j-1], frames[:, :, j])
+        ax.imshow(np.ma.masked_array(mask, np.invert(mask)), alpha=0.7, cmap='autumn')
+
+        return im
+
+    # Run animation and display window
+    ani = animation.FuncAnimation(fig, updatefig, frames=range(1, total_frames), interval=1000//fps)
+    plt.show()
+
+if __name__ == '__main__':
+    play('../data/aerialseq.npy')

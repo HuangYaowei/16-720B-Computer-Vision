145a8a89969b373686120c332fd91d916cbdb6ba
heethesh <heethesh@gmail.com>
2018-10-23 04:20:18 -0400

    Remove error normalization

--- Assignments/HW3/code/SubtractDominantMotion.py
+++ Assignments/HW3/code/SubtractDominantMotion.py
@@ -18,7 +18,7 @@ from scipy.ndimage.morphology import binary_opening, binary_closing
 from LucasKanade import disp
 from LucasKanadeAffine import LucasKanadeAffine
 
-def SubtractDominantMotion(image1, image2, LK_method=LucasKanadeAffine, threshold=0.3, iters=7):
+def SubtractDominantMotion(image1, image2, LK_method=LucasKanadeAffine, threshold=0.15, iters=7):
     '''
     [input]
     * image1 - Image at time t
@@ -40,9 +40,8 @@ def SubtractDominantMotion(image1, image2, LK_method=LucasKanadeAffine, threshol
     warp_img = affine_transform(image1, M2to1)
     aff_mask = affine_transform(np.ones(image1.shape), M2to1)
     
-    # Get difference image and normalize it
+    # Get difference image
     diff = np.abs((aff_mask * image2) - (aff_mask * warp_img))
-    diff = (diff - diff.min()) * (1/(diff.max() - diff.min()))
     
     # Create a binary mask using the theshold
     mask = np.where(diff > threshold, 1, 0).astype('bool')

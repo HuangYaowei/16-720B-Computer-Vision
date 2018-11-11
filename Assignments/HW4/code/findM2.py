b69378801b10728fe033dbefe75d14ae7c6176b0
heethesh <heethesh@gmail.com>
2018-11-11 04:40:07 -0500

    Completed Q3

--- Assignments/HW4/code/findM2.py
+++ Assignments/HW4/code/findM2.py
@@ -1,6 +1,48 @@
+import numpy as np
+
+import helper
+import submission as sub
+
 '''
 Q3.3:
     1. Load point correspondences
     2. Obtain the correct M2
     3. Save the correct M2, C2, p1, p2, R and P to q3_3.mat
 '''
+def findM2():
+    # Load data
+    some_corresp = np.load('../data/some_corresp.npz')
+    intrinsics = np.load('../data/intrinsics.npz')
+
+    # Compute fundamental and essential matrices
+    F = sub.eightpoint(some_corresp['pts1'], some_corresp['pts2'], 640)
+    E = sub.essentialMatrix(F, intrinsics['K1'], intrinsics['K2'])
+
+    # Copmute camera 1 matrix
+    M1 = np.concatenate([np.eye(3), np.zeros([3, 1])], axis=1)
+    C1 = intrinsics['K1'] @ M1
+    
+    # Find the correct M2
+    best_data = None
+    best_err = np.finfo('float').max
+    M2s = helper.camera2(E)
+    
+    for i in range(4):
+        # Compute camera 2 matrix
+        C2 = intrinsics['K2'] @ M2s[:, :, i]
+        P, err = sub.triangulate(C1, some_corresp['pts1'], C2, some_corresp['pts2'])
+
+        # Ensure all Z values are positive        
+        if len(P[P[:, 2] > 0]) != P.shape[0]: continue
+
+        # Save the correct data
+        if err < best_err:
+            best_err = err
+            best_data = [M2s[:, :, i], C2, P]
+
+    # Save the data
+    np.savez('q3_3', M2=best_data[0], C2=best_data[1], P=best_data[2])
+
+if __name__ == '__main__':
+    findM2()
+    
--- Assignments/HW4/code/submission.py
+++ Assignments/HW4/code/submission.py
@@ -4,7 +4,8 @@ Replace 'pass' by your implementation
 """
 
 # Insert your package here
-import cv2
+import sys
+
 import numpy as np
 import scipy.ndimage
 import matplotlib.pyplot as plt
@@ -46,7 +47,7 @@ Q2.2: Seven Point Algorithm
     Input:  pts1, Nx2 Matrix
             pts2, Nx2 Matrix
             M, a scalar parameter computed as max (imwidth, imheight)
-    Output: Farray, a list of estimated fundamental matrix.
+    Output: Farray, a list of estimated fundamental matrix
 '''
 def sevenpoint(pts1, pts2, M):
     assert(pts1.shape[0] == 7)
@@ -88,7 +89,7 @@ def sevenpoint(pts1, pts2, M):
     return Farray
 
 '''
-Q3.1: Compute the essential matrix E.
+Q3.1: Compute the essential matrix E
     Input:  F, fundamental matrix
             K1, internal camera calibration matrix of camera 1
             K2, internal camera calibration matrix of camera 2
@@ -98,21 +99,37 @@ def essentialMatrix(F, K1, K2):
     return K2.T @ F @ K1 
 
 '''
-Q3.2: Triangulate a set of 2D coordinates in the image to a set of 3D points.
+Q3.2: Triangulate a set of 2D coordinates in the image to a set of 3D points
     Input:  C1, the 3x4 camera matrix
             pts1, the Nx2 matrix with the 2D image coordinates per row
             C2, the 3x4 camera matrix
             pts2, the Nx2 matrix with the 2D image coordinates per row
     Output: P, the Nx3 matrix with the corresponding 3D points per row
-            err, the reprojection error.
+            err, the reprojection error
 '''
 def triangulate(C1, pts1, C2, pts2):
-    # Replace pass by your implementation
-    pass
+    err = 0
+    P = []
 
+    for i in range(pts1.shape[0]):
+        A = [pts1[i, 0] * C1[2, :] - C1[0, :],
+             pts1[i, 1] * C1[2, :] - C1[1, :],
+             pts2[i, 0] * C2[2, :] - C2[0, :],
+             pts2[i, 1] * C2[2, :] - C2[1, :]]
+
+        U, S, V = np.linalg.svd(np.asarray(A))
+        w = V[-1, :]/V[-1, -1]
+        P.append(w[:3])
+
+        proj1 = C1 @ w
+        proj2 = C2 @ w
+
+        err += np.linalg.norm(proj1[:2]/proj1[-1] - pts1)**2 + np.linalg.norm(proj2[:2]/proj2[-1] - pts2)**2
+        
+    return np.asarray(P), err
 
 '''
-Q4.1: 3D visualization of the temple images.
+Q4.1: 3D visualization of the temple images
     Input:  im1, the first image
             im2, the second image
             F, the fundamental matrix
@@ -120,14 +137,13 @@ Q4.1: 3D visualization of the temple images.
             y1, y-coordinates of a pixel on im1
     Output: x2, x-coordinates of the pixel on im2
             y2, y-coordinates of the pixel on im2
-
 '''
 def epipolarCorrespondence(im1, im2, F, x1, y1):
     # Replace pass by your implementation
     pass
 
 '''
-Q5.1: RANSAC method.
+Q5.1: RANSAC method
     Input:  pts1, Nx2 Matrix
             pts2, Nx2 Matrix
             M, a scaler parameter
@@ -138,7 +154,7 @@ def ransacF(pts1, pts2, M):
     pass
 
 '''
-Q5.2: Rodrigues formula.
+Q5.2: Rodrigues formula
     Input:  r, a 3x1 vector
     Output: R, a rotation matrix
 '''
@@ -147,7 +163,7 @@ def rodrigues(r):
     pass
 
 '''
-Q5.2: Inverse Rodrigues formula.
+Q5.2: Inverse Rodrigues formula
     Input:  R, a rotation matrix
     Output: r, a 3x1 vector
 '''
@@ -156,21 +172,21 @@ def invRodrigues(R):
     pass
 
 '''
-Q5.3: Rodrigues residual.
+Q5.3: Rodrigues residual
     Input:  K1, the intrinsics of camera 1
             M1, the extrinsics of camera 1
             p1, the 2D coordinates of points in image 1
             K2, the intrinsics of camera 2
             p2, the 2D coordinates of points in image 2
-            x, the flattened concatenationg of P, r2, and t2.
-    Output: residuals, the difference between original and estimated projections
+            x, the flattened concatenationg of P, r2, and t2
+                Output: residuals, the difference between original and estimated projections
 '''
 def rodriguesResidual(K1, M1, p1, K2, p2, x):
     # Replace pass by your implementation
     pass
 
 '''
-Q5.3 Bundle adjustment.
+Q5.3 Bundle adjustment
     Input:  K1, the intrinsics of camera 1
             M1, the extrinsics of camera 1
             p1, the 2D coordinates of points in image 1
@@ -186,14 +202,24 @@ def bundleAdjustment(K1, M1, p1, K2, M2_init, p2, P_init):
     pass
 
 if __name__ == '__main__':
+    # Load data
     some_corresp = np.load('../data/some_corresp.npz')
+    intrinsics = np.load('../data/intrinsics.npz')
+
+    # Load images
     im1 = scipy.ndimage.imread('../data/im1.png')
-    im2 = scipy.ndimage.imread('../data/im2.png')    
-    
-    # 4-0
+    im2 = scipy.ndimage.imread('../data/im2.png')
+    M = max(im1.shape)
+
+    # Eight-point algorithm
+    F = eightpoint(some_corresp['pts1'], some_corresp['pts2'], M)
+    np.savez('q2_1', F=F, M=M)
+
+    # Seven-point algorithm
     np.random.seed(4)
     r = np.random.randint(0, some_corresp['pts1'].shape[0], 7)
-
-    # F = eightpoint(some_corresp['pts1'], some_corresp['pts2'], max(im1.shape))
-    F = sevenpoint(some_corresp['pts1'][r], some_corresp['pts2'][r], max(im1.shape))
+    F = sevenpoint(some_corresp['pts1'][r], some_corresp['pts2'][r], M)
+    np.savez('q2_2', F=F[0], M=M, pts1=some_corresp['pts1'][r], pts2=some_corresp['pts2'][r])
+    
+    # Display on GUI
     helper.displayEpipolarF(im1, im2, F[0])

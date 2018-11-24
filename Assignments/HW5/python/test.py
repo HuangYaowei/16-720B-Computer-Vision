7247f09369a50290991e12cbe47931ffd2effa5a
heethesh <heethesh@gmail.com>
2018-11-24 03:37:34 -0500

    $ git creeped in, why is this script here?? :/

--- Assignments/HW5/python/test.py
+++ Assignments/HW5/python/test.py
@@ -1,4 +1,6 @@
 import numpy as np
+import skimage.io
+
 from nn import *
 from util import *
 
@@ -30,4 +32,32 @@ def test_softmax_basic():
     assert np.amax(np.fabs(test3 - np.array(
         [0.73105858, 0.26894142]))) <= 1e-6
 
-test_softmax_basic()
+# test_softmax_basic()
+
+# avg = 0
+# for i in range(1, 5):
+#     a = i*2
+#     avg = (a + avg*(i-1))/(i)
+#     print(a, avg)
+
+# print(avg)
+
+# i = 0
+# l = [1, 3, 2, 4, 5, 6, 7, 8]
+# while i < len(l):
+#     print(l[i])
+#     if l[i] == 2:
+#         l = l[:i] + [20, 21] + l[i+1:]
+#         i += 2
+#     i += 1
+# print(l)
+
+iters = lambda img: (img.shape[0] * img.shape[1])//int(1.5e6) + 3*int(img.shape[1]>4000)
+img = skimage.io.imread('../images/01_list.jpg')
+print(iters(img))
+img = skimage.io.imread('../images/02_letters.jpg')
+print(iters(img))
+img = skimage.io.imread('../images/03_haiku.jpg')
+print(iters(img))
+img = skimage.io.imread('../images/04_deep.jpg')
+print(iters(img))

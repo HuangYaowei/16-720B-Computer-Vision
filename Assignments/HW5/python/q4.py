96b79f60a93d706265e8f27cd8345212068a3667
heethesh <heethesh@gmail.com>
2018-11-24 03:36:05 -0500

    Text extraction complete! ABC123

--- Assignments/HW5/python/q4.py
+++ Assignments/HW5/python/q4.py
@@ -1,20 +1,115 @@
 import numpy as np
-
 import skimage
+import skimage.io
 import skimage.measure
 import skimage.color
-import skimage.restoration
 import skimage.filters
 import skimage.morphology
 import skimage.segmentation
 
-# takes a color image
-# returns a list of bounding boxes and black_and_white image
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+BLUR_SIGMA = 3
+BW_CLOSING_ITERS = 3
+MIN_BBOX_AREA = 1000
+LABELS_EROSION_ITERS = 4
+JOINT_MIN_ASPECT_RATIO = 1.15
+JOINT_EROSION_ITERS = 1
+LINE_DETECTION_THRESH = 100
+
+def findLabels(image, morph_iters=LABELS_EROSION_ITERS):
+    for _ in range(morph_iters): image = skimage.morphology.binary_erosion(image)
+    image = skimage.img_as_float(image)
+    labels = skimage.measure.label(image-1)
+    regions = skimage.measure.regionprops(labels)
+    return labels, regions
+
+def drawBoxes(image, bboxes):
+    fig, ax = plt.subplots()
+    ax.imshow(image, cmap='gray')
+
+    for bbox in bboxes:
+        minr, minc, maxr, maxc = bbox
+        rect = mpatches.Rectangle((minc, minr), maxc-minc, maxr-minr, fill=False, edgecolor='red', linewidth=1)
+        ax.add_patch(rect)
+
+    plt.show()
+
+def removeJoints(image, bboxes):
+    i = 0
+    while i < len(bboxes):
+        minr, minc, maxr, maxc = bboxes[i]
+        if (maxc - minc) / (maxr - minr) > JOINT_MIN_ASPECT_RATIO:
+            letter = np.ones_like(image)
+            letter[minr:maxr, minc:maxc] = image[minr:maxr, minc:maxc]
+            _, regions = findLabels(letter, morph_iters=JOINT_EROSION_ITERS)
+            n = len(regions)
+            if n > 1:
+                print('Found %d joined letters'%n)
+                bboxes = bboxes[:i] + [ region.bbox for region in regions ] + bboxes[i+1:]
+                i += (n - 1)
+        i += 1
+
+    return bboxes
+
+def sortLetters(bboxes):
+    # Sort all letters row wise
+    bboxes.sort(key=lambda x: (x[0]))
+    
+    # Find lines and group the letters per line
+    lines, words = [], []
+    last_line = 1e8
+    for bbox in bboxes:
+        if abs(last_line - bbox[0]) > LINE_DETECTION_THRESH:
+            if words: lines.append(words)
+            words = []
+        words.append(bbox)
+        last_line = bbox[0]
+    if words: lines.append(words)
+
+    # Sort the letters column wise per line
+    counts = []
+    bboxes_sorted = []
+    for line in lines:
+        line.sort(key=lambda x: (x[1]))
+        counts.append(len(line))
+        bboxes_sorted += line
+
+    return bboxes_sorted, np.cumsum(counts)
+    
 def findLetters(image):
+    image = skimage.img_as_float(image)
+    image = skimage.color.rgb2gray(image)
+    image = image / np.max(image)
+    image = skimage.filters.gaussian(image, sigma=BLUR_SIGMA)
+    thresh = skimage.filters.threshold_minimum(image)
+    
+    bw = image > thresh
+    iters = (image.shape[0] * image.shape[1])//int(1.5e6) + 2*int(image.shape[1]>4000)
+    for _ in range(iters): bw = skimage.morphology.binary_erosion(bw)
+    for _ in range(BW_CLOSING_ITERS): bw = skimage.morphology.binary_closing(bw)
+    bw = skimage.img_as_float(bw)
+
+    labels, regions = findLabels(bw)
+
+    lines = 0
     bboxes = []
-    bw = None
-    # insert processing in here
-    # one idea estimate noise -> denoise -> greyscale -> threshold -> morphology -> label -> skip small boxes 
-    # this can be 10 to 15 lines of code using skimage functions
+    for region in regions:
+        if region.area > MIN_BBOX_AREA:
+            minr, minc, maxr, maxc = region.bbox
+            bboxes.append(region.bbox)
+
+    bboxes = removeJoints(bw, bboxes)
+    if __name__ == '__main__': drawBoxes(bw, bboxes)
     
     return bboxes, bw
+
+if __name__ == '__main__':
+    n = 4
+    # n = np.random.randint(1, 5)
+    if n==1: img = skimage.io.imread('../images/01_list.jpg')
+    if n==2: img = skimage.io.imread('../images/02_letters.jpg')
+    if n==3: img = skimage.io.imread('../images/03_haiku.jpg')
+    if n==4: img = skimage.io.imread('../images/04_deep.jpg')
+    findLetters(img)

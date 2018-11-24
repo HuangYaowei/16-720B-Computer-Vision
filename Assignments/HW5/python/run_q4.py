eb8a9b1000baea10845b294b59d313f430f05602
heethesh <heethesh@gmail.com>
2018-11-24 03:36:54 -0500

    Text detection complete

--- Assignments/HW5/python/run_q4.py
+++ Assignments/HW5/python/run_q4.py
@@ -1,46 +1,80 @@
 import os
+import pickle
+import string
+
 import numpy as np
+import skimage
+import skimage.io
+import skimage.transform
 import matplotlib.pyplot as plt
 import matplotlib.patches
 
-import skimage
-import skimage.measure
-import skimage.color
-import skimage.restoration
-import skimage.io
-import skimage.filters
-import skimage.morphology
-import skimage.segmentation
-
 from nn import *
 from q4 import *
-# do not include any more libraries here!
-# no opencv, no sklearn, etc!
+
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=UserWarning)
 
-for img in os.listdir('../images'):
-    im1 = skimage.img_as_float(skimage.io.imread(os.path.join('../images',img)))
-    bboxes, bw = findLetters(im1)
+ground_thruth = [list('TODOLIST1MAKEATODOLIST2CHECKOFFTHEFIRSTTHINGONTODOLIST3REALIZEYOUHAVEALREADYCOMPLETED2THINGS4REWARDYOURSELFWITHANAP'),
+                 list('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'),
+                 list('HAIKUSAREEASYBUTSOMETIMESTHEYDONTMAKESENSEREFRIGERATOR'),
+                 list('DEEPLEARNINGDEEPERLEARNINGDEEPESTLEARNING')]
 
-    plt.imshow(bw)
-    for bbox in bboxes:
+for img in os.listdir('../images'):
+    # Read the test image
+    im1 = skimage.img_as_float(skimage.io.imread(os.path.join('../images', img)))
+    print('\nDetecting >>>', img)
+    
+    # Find and sort letters per line
+    bboxes, bw = findLetters(im1)
+    bboxes, counts = sortLetters(bboxes)
+    # plt.imshow(bw)
+
+    # Preprocessing
+    letters_to_detect = []
+    for i, bbox in enumerate(bboxes):
         minr, minc, maxr, maxc = bbox
-        rect = matplotlib.patches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                                fill=False, edgecolor='red', linewidth=2)
-        plt.gca().add_patch(rect)
-    plt.show()
-    # find the rows using..RANSAC, counting, clustering, etc.
+
+        # Rescale and crop letters
+        crop = bw[minr:maxr, minc:maxc]
+        crop = skimage.transform.rescale(crop, 29/max(maxc-minc, maxr-minr))
+        
+        # Pad letters with odd shape offsets
+        ypad, xpad = (32 - crop.shape[0])//2, (32 - crop.shape[1])//2
+        yodd, xodd = int(crop.shape[0]%2!=0), int(crop.shape[1]%2!=0)
+        letter = np.pad(crop, ((ypad, ypad + 1*yodd), (xpad, xpad + 1*xodd)), 'constant', constant_values=(1, 1))
+        skimage.io.imsave('../crops/%s_%02d.png'%(img.split('.')[0], i), letter)
+
+        # Transpose and add letter for detection
+        letters_to_detect.append(letter.T.flatten())
+
+        # Draw bounding boxes
+        rect = matplotlib.patches.Rectangle((minc, minr), maxc-minc, maxr-minr, fill=False, edgecolor='red', linewidth=2)
+        # plt.gca().add_patch(rect)
     
-    # crop the bounding boxes
-    # note.. before you flatten, transpose the image (that's how the dataset is!)
-    # consider doing a square crop, and even using np.pad() to get your images looking more like the dataset
+    # Display the image
+    # plt.show()
+
+    # Test data
+    letters_to_detect = np.vstack(letters_to_detect)
     
-    # load the weights
-    # run the crops through your neural network and print them out
-    import pickle
-    import string
+    # Load the weights
+    params = pickle.load(open('q3_weights.pickle', 'rb'))
+    
+    # Detect the letters using the neural network
+    h1 = forward(letters_to_detect, params, name='layer1', activation=sigmoid)
+    probs = forward(h1, params, name='output', activation=softmax)
+    detects = np.argmax(probs, axis=1)
+
+    # Infer the detections
     letters = np.array([_ for _ in string.ascii_uppercase[:26]] + [str(_) for _ in range(10)])
-    params = pickle.load(open('q3_weights.pickle','rb'))
-    
+    lines = np.split(letters[detects], counts[:-1])
+    lines = [ ''.join(line.tolist()) for line in lines ]
+
+    # Calculate accuracy 
+    correct = (ground_thruth[int(img[1])-1] == letters[detects])
+    print('Accuracy: %.2f%%\n'%(correct.sum()*100.0/correct.size))
+
+    # Display the detected lines
+    for line in lines: print(line)

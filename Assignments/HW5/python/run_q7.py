59cb4e10d9414fe95a0dd44045095819dfd1fb3d
heethesh <heethesh@gmail.com>
2018-11-28 01:41:13 -0500

    Support for train and val

--- Assignments/HW5/python/run_q7.py
+++ Assignments/HW5/python/run_q7.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
@@ -5,13 +6,10 @@ import torch.nn.functional as F
 def accuracy(probs, y):
     probs = F.softmax(probs, dim=1)
     correct = (torch.max(probs, 1)[1] == torch.max(y, 1)[1]).sum()
-    acc = correct.float() / y.size()[0]
+    acc = correct.float() / y.size(0)
     return acc.item()
 
-def train():
-    # Network model
-    net = Net()
-
+def train(save=False):
     # Optimizer function
     optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
     
@@ -19,6 +17,7 @@ def train():
     all_loss, all_acc = [], []
     for itr in range(max_iters):
         total_loss, total_acc = 0, 0
+        net.train()
         for xb, yb in batches:
             # Zero the parameter gradients
             optimizer.zero_grad()
@@ -37,6 +36,7 @@ def train():
         avg_acc = total_acc / batch_num
 
         # Validation forward pass, loss and accuracy
+        net.eval()
         voutputs = net(valid_x)
         valid_loss = F.cross_entropy(voutputs, torch.max(valid_y, 1)[1])
         valid_acc = accuracy(voutputs, valid_y)
@@ -45,18 +45,29 @@ def train():
         all_loss.append([total_loss, valid_loss])
         all_acc.append([avg_acc, valid_acc])
 
-        if itr % 2 == 0:
+        if itr % 1 == 0:
             print("itr: {:03d} loss: {:.2f} acc: {:.2f} vloss: {:.2f} vacc: {:.2f}".format(itr, total_loss, avg_acc, valid_loss, valid_acc))
 
+    # Save the learned parameters
+    if save: 
+        torch.save(net.state_dict(), 'q7_%s.pt'%model_name)
+        np.savez('q7_%s_lossacc'%model_name, loss=np.asarray(all_loss), accuracy=np.asarray(all_acc))
+    
     return all_loss, all_acc
 
 if __name__ == '__main__':
     # Select model
-    # from fcn_nist36 import *
-    from cnn_mnist import *
-    # from cnn_nist36 import *
-    # from cnn_emnist import *
+    mode = 5
+    if mode==1: from fcn_nist36 import *
+    if mode==2: from cnn_mnist import *
+    if mode==3: from cnn_nist36 import *
+    if mode==4: from cnn_emnist import *
+    if mode==5: from lenet5_flowers import *
+    if mode==0: from squeezenet_flowers import *
+
+    # Network model
+    if mode: net = Net()
 
     # Training loop
-    train()
+    train(save=True)
     

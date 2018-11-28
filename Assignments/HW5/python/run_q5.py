0cdfaa9af56c384163f01a887c9a89dd6796ed01
heethesh <heethesh@gmail.com>
2018-11-28 01:40:57 -0500

    Fixed autoencoder

--- Assignments/HW5/python/run_q5.py
+++ Assignments/HW5/python/run_q5.py
@@ -1,6 +1,3 @@
-import sys
-from collections import Counter
-
 import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,8 +12,8 @@ train_x = train_data['train_data']
 valid_x = valid_data['valid_data']
 
 # Hyperparameters
-max_iters = 100
-learning_rate = 3e-3 # 3e-5
+max_iters = 2
+learning_rate = 3e-5
 momentum = 0.9
 lr_rate = 20
 hidden_size = 32
@@ -37,6 +34,7 @@ for layer in ['output', 'layer1', 'hidden', 'hidden2']:
     params['m_' + layer] = np.zeros_like(params['W' + layer])
 
 # Training loop
+all_loss = []
 for itr in range(max_iters):
     total_loss = 0
     for xb, _ in batches:
@@ -47,10 +45,11 @@ for itr in range(max_iters):
         out = forward(h3, params, 'output', sigmoid)
 
         # Loss function
-        total_loss += np.sum((xb - out)**2)
+        error = xb - out
+        total_loss += np.sum(error**2)
 
         # Backward pass
-        delta = 2*(xb - out)
+        delta = -2 * error
         delta1 = backwards(delta, params, 'output', sigmoid_deriv)
         delta2 = backwards(delta1, params, 'hidden2', relu_deriv)
         delta3 = backwards(delta2, params, 'hidden', relu_deriv)
@@ -62,27 +61,53 @@ for itr in range(max_iters):
             params['W' + layer] += params['m_' + layer]
             params['b' + layer] -= learning_rate * params['grad_b' + layer]
     
+    # Save loss
+    all_loss.append(total_loss)
+
     if itr % 2 == 0:
         print("itr: {:02d} lr: {:.6f} loss: {:.2f}".format(itr, learning_rate, total_loss))
     
     if itr % lr_rate == lr_rate-1:
         learning_rate *= momentum
 
-sys.exit(0)
-
 # Q5.3.1
-h1 = forward(xb, params, 'layer1', relu)
+indices = [0, 1, 1000, 1001, 3000, 3001, 5000, 5001, 7000, 7001]
+h1 = forward(train_x[indices], params, 'layer1', relu)
 h2 = forward(h1, params, 'hidden', relu)
 h3 = forward(h2, params, 'hidden2', relu)
 out = forward(h3, params, 'output', sigmoid)
 
 # Visualize some results
-for i in range(5):
-    plt.subplot(2, 1, 1)
-    plt.imshow(xb[i].reshape(32, 32).T)
-    plt.subplot(2, 1, 2)
-    plt.imshow(out[i].reshape(32, 32).T)
+fig, ax = plt.subplots(2, 2, sharex=True, sharey=True)
+for i in range(0, 10, 2):
+    plt.subplot(2, 2, 1)
+    plt.imshow(train_x[indices[i]].reshape(32, 32).T, cmap='gray')
+    plt.subplot(2, 2, 3)
+    plt.imshow(out[i].reshape(32, 32).T, cmap='gray')
+    plt.subplot(2, 2, 2)
+    plt.imshow(train_x[indices[i+1]].reshape(32, 32).T, cmap='gray')
+    plt.subplot(2, 2, 4)
+    plt.imshow(out[i+1].reshape(32, 32).T, cmap='gray')
     plt.show()
 
+# Plot epoch vs loss
+plt.plot(np.arange(max_iters), all_loss)
+plt.title('Training Loss Curve')
+plt.xlabel('Epochs')
+plt.ylabel('Total Loss')
+plt.show()
+
 # Q5.3.2
-# Evaluate PSNR
+psnrs = 0
+for xb in valid_x:
+    # Forward pass
+    h1 = forward(xb, params, 'layer1', relu)
+    h2 = forward(h1, params, 'hidden', relu)
+    h3 = forward(h2, params, 'hidden2', relu)
+    out = forward(h3, params, 'output', sigmoid)
+    
+    # Evaluate PSNR
+    psnrs += psnr(xb, out)
+
+# Average PSNR
+print(psnrs/valid_x.shape[0])

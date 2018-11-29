0090aed2fb4f975b670d6278d1b79374b630fc30
heethesh <heethesh@gmail.com>
2018-11-29 00:04:29 -0500

    Minor changes

--- Assignments/HW5/python/grid.py
+++ Assignments/HW5/python/grid.py
@@ -12,38 +12,36 @@ from natsort import natsorted, ns
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 
-def thumb_grid(im_list, grid_shape, scale=1, axes_pad=0):
-    # Grid must be 2D:
+def display_grid(im_list, grid_shape, axes_pad=0, pad_image=False, cmap='gray'):
+    # Grid must be 2D
     assert len(grid_shape) == 2
 
-    # Make sure all images can fit in grid:
+    # Make sure all images can fit in grid
     assert np.prod(grid_shape) >= len(im_list)
 
     grid = ImageGrid(plt.gcf(), 111, grid_shape, axes_pad=axes_pad)
-    for i in range(N):
+    for i in range(len(im_list)):
         data_orig = im_list[i]
 
-        # Scale image:
+        # Scale image
         im = PIL.Image.fromarray(data_orig)
-        data_thumb = np.array(im)
-        data_thumb = np.pad(data_thumb, ((3, 3), (3, 3)), 'constant', constant_values=(1, 1))
-        grid[i].imshow(data_thumb, cmap='gray')
+        data = np.array(im)
+        if pad_image: data = np.pad(data, ((3, 3), (3, 3)), 'constant', constant_values=(1, 1))
+        grid[i].imshow(data, cmap=cmap)
 
-        # Turn off axes:
+        # Turn off axes
         grid[i].axes.get_xaxis().set_visible(False)
         grid[i].axes.get_yaxis().set_visible(False)
+    
+    # Display grid
+    plt.show()
         
 if __name__ == '__main__':
-
     file_list = natsorted(os.listdir('../crops'), alg=ns.IGNORECASE)
     im_list = [ skimage.img_as_float(skimage.io.imread(os.path.join('../crops', img))) for img in file_list ]
         
     # 233 * 7 = 18 * 7 = 126
-    N = len(im_list[:18*7])
-    thumb_grid(im_list[:18*7], (7, 18))
-    plt.show()
+    display_grid(im_list[:18*7], (7, 18), pad_image=True)
 
     # 35 * 222 = 15 * 8 = 120
-    N = len(im_list[18*7:])
-    thumb_grid(im_list[18*7:], (8, 15))
-    plt.show()
+    display_grid(im_list[18*7:], (8, 15), pad_image=True)
